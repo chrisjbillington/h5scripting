ffbc38608757a3961a4b49bed08ca152b775ea15
Chris Billington <chrisjbillington@gmail.com>
2014-11-05 17:32:04 -0500

    Removed left in lines only required for my debugging

--- h5scripting.py
+++ h5scripting.py
@@ -10,8 +10,6 @@ to an h5 file and allowing these to be executed in a nice way.
 One intended use of this module is embedding the function needed to generate
 a plot from data within an h5 file.
 """
-import matplotlib
-matplotlib.use("QT4Agg")
 
 import sys
 import h5py
