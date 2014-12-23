5322b48dc0e6ffb0a51301f6e82c564370a67c1e
Chris Billington <chrisjbillington@gmail.com>
2014-12-23 14:25:49 +1100

    Restructured for release as version 0.1.0
    Added standard files for realease: LICENSE.txt containing
    the 2 clause BSD license, MANIFEST.in, README.md and setup.py.
    
    Added an hgignore which ignores the autogenerated __version__.py.
    
    Added __version__ module attribute to __init__.py such that
    h5scripting.__version__ gives the version string

--- h5scripting/__init__.py
+++ h5scripting/__init__.py
@@ -5,4 +5,9 @@ Created on Wed Nov 12 12:02:51 2014
 @author: ispielma
 """
 
+try:
+    from __version__ import __version__
+except ImportError:
+    __version__ = None
+
 from .h5scripting import *
